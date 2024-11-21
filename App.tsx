71c7886579deb5cc45bc7739b8168e7a210f7d8a
Malik <mosaddekamallick@gmail.com>
2024-11-21 22:09:44 +0530

    BookTrack to Book

--- App.tsx
+++ App.tsx
@@ -2,20 +2,17 @@ import { useState, useEffect } from "react";
 import { supabase } from "./lib/supabase";
 import Auth from "./components/Auth";
 import Account from "./components/Account";
-import { View, Text} from "react-native";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Session } from "@supabase/supabase-js";
-
-import { Ionicons} from "@expo/vector-icons";
-import BookTrack from "./components/BookTrack/BookTrack";
+import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./components/Home/HomeScreen";
+import Book from "./components/Book/Book";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
-
-
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -52,8 +49,8 @@ export default function App() {
         }}
       />
       <Tab.Screen
-        name="BookTrack"
-        component={BookTrack}
+        name="Book"
+        component={Book}
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="book" size={size} color={color} />
---
+++ components/Book/Book.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Book() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Book</Text>
+    </View>
+  );
+}
