102001b1c43310d9d9b4a1d3e35e08ece8a89e11
Malik <mosaddekamallick@gmail.com>
2024-11-21 13:34:27 +0530

    Created bottom tab

--- App.tsx
+++ App.tsx
@@ -1,53 +1,26 @@
-import { useState, useEffect } from 'react';
-import { supabase } from './lib/supabase';
-import Auth from './components/Auth';
-import Account from './components/Account';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Session } from '@supabase/supabase-js';
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Account from "./components/Account";
+import { View, Text} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Session } from "@supabase/supabase-js";
 
+import { Ionicons} from "@expo/vector-icons";
+import BookTrack from "./components/BookTrack/BookTrack";
+
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
-function HomeScreen() {
+function HomeScreen({ navigation }: { navigation: any }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home Screen</Text>
     </View>
   );
 }
-
-const AccountStack = ({ session }: { session: Session }) => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="AccountMain"
-      children={() => <Account session={session} />} 
-      options={({ navigation }) => ({
-        title: 'Account',
-        headerRight: () => (
-          <Text
-            style={{ marginRight: 10, color: 'blue' }}
-            onPress={() => navigation.goBack()}
-          >
-            ✖ Close
-          </Text>
-        ),
-      })}
-    />
-  </Stack.Navigator>
-);
-
-const AppDrawer = ({ session }: { session: Session }) => (
-  <Drawer.Navigator>
-    <Drawer.Screen name="Home" component={HomeScreen} />
-    <Drawer.Screen
-      name="Account"
-      children={() => <AccountStack session={session!} />}
-    />
-  </Drawer.Navigator>
-);
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
@@ -64,13 +37,49 @@ export default function App() {
 
   const AuthStack = () => (
     <Stack.Navigator>
-      <Stack.Screen name="SignIn" component={Auth} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="SignIn"
+        component={Auth}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
+  );
+
+  const AppTabs = ({ session }: { session: Session }) => (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="BookTrack"
+        component={BookTrack}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        children={(props) => <Account {...props} session={session} />}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 
   return (
     <NavigationContainer>
-      {session && session.user ? <AppDrawer session={session} /> : <AuthStack />}
+      {session && session.user ? <AppTabs session={session} /> : <AuthStack />}
     </NavigationContainer>
   );
 }
---
+++ components/BookTrack/BookTrack.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function BookTrack() {
+  return (
+    <View>
+      <Text>BookTrack</Text>
+    </View>
+  )
+}
